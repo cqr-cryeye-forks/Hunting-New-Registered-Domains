3cec058995d8c80757f6836705e9e65a9d1d3a51
Alex-Matveenko <matveenko.a.p@gmail.com>
2023-04-13 14:28:00 +0300

    add path to write output

---
+++ config.py
@@ -0,0 +1,17 @@
+import pathlib
+from typing import Final
+
+from init_args import args
+
+ROOT_PATH: Final[pathlib.Path] = pathlib.Path(__file__).parent
+RESULT_STORAGE: Final[pathlib.Path] = ROOT_PATH.joinpath("result_storage")
+RESULT_STORAGE.mkdir(exist_ok=True)
+
+DNS_FILE: Final[pathlib.Path] = RESULT_STORAGE.joinpath("dns_scan.json")
+IP2ASN_FILE: Final[pathlib.Path] = RESULT_STORAGE.joinpath("ip2asn_scan.json")
+CERTIFICATES_FILE: Final[pathlib.Path] = RESULT_STORAGE.joinpath("certificates_scan.json")
+VIRUS_TOTAL_FILE: Final[pathlib.Path] = RESULT_STORAGE.joinpath("virus_total_scan.json")
+QUAD9_FILE: Final[pathlib.Path] = RESULT_STORAGE.joinpath("quad9_scan.json")
+SHANNON_ENTROPY_FILE: Final[pathlib.Path] = RESULT_STORAGE.joinpath("shannon_entropy_scan.json")
+
+FINAL_RESULT_FILE: Final[pathlib.Path] = RESULT_STORAGE.joinpath(args.output)
